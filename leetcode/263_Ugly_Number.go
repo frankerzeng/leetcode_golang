aa92420558ad887088b3d40f7f9e5176b0c3b587
zengfanlong1 <zengfanlong1@globalegrow.com>
2018-08-21 14:31:31 +0800

    263. Ugly Number

--- leetcode/263_Ugly_Number.go
+++ leetcode/263_Ugly_Number.go
@@ -24,3 +24,39 @@ Note:
 	2 Input is within the 32-bit signed integer range: [−231,  231 − 1].
 */
 package main
+
+func main() {
+	println(isUgly(14))
+}
+
+func isUgly(num int) bool {
+	dividedFun := func(dividend int, divider int) int {
+		if dividend%divider == 0 {
+			return dividend / divider
+		} else {
+			return 0
+		}
+	}
+	if num < 1 {
+		return false
+	}
+	if num == 1 {
+		return true
+	}
+	// can divided by x 2 and y 3 and z 5
+	for _, v := range []int{2, 3, 5} {
+		for true {
+			rst := dividedFun(num, v)
+			if rst == 1 {
+				return true
+			}
+			if rst > 0 {
+				num = rst
+				continue
+			}
+			break
+		}
+	}
+
+	return false
+}
