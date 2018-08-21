9a9b9b6b0f075bd4f7f62fe437d99f41526793c8
frankerzeng <zengfanlong1@globalegrow.com>
2018-08-21 16:54:09 +0800

    701. Insert into a Binary Search Tree

---
+++ package1/330_Patching_Array.go
@@ -0,0 +1,50 @@
+/**
+330. Patching Array
+Given a sorted positive integer array nums and an integer n, add/patch elements to the array such that any number in range
+[1, n] inclusive can be formed by the sum of some elements in the array. Return the minimum number of patches required.
+
+Example 1:
+
+Input: nums = [1,3], n = 6
+Output: 1
+Explanation:
+Combinations of nums are [1], [3], [1,3], which form possible sums of: 1, 3, 4.
+Now if we add/patch 2 to nums, the combinations are: [1], [2], [3], [1,3], [2,3], [1,2,3].
+Possible sums are 1, 2, 3, 4, 5, 6, which now covers the range [1, 6].
+So we only need 1 patch.
+Example 2:
+
+Input: nums = [1,5,10], n = 20
+Output: 2
+Explanation: The two patches can be [2, 4].
+Example 3:
+
+Input: nums = [1,2,2], n = 5
+Output: 0
+*/
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 1, 6}
+	n := 26
+	fmt.Println(minPatches(nums, n))
+}
+func minPatches(nums []int, n int) int {
+	miss := 1 // max value can reached
+	added := 0
+	i := 0
+	for miss <= n {
+		if i < len(nums) && nums[i] <= miss {
+			miss += nums[i]
+			i++
+		} else {
+			miss += miss
+			added++
+		}
+		println(i, miss)
+	}
+
+	return added
+}
