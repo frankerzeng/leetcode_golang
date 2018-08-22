f6dac5059b67422f0e9b11ad4706eeec07b119a8
frankerzeng <frankerzeng@163.com>
2018-08-22 18:00:09 +0800

    132. Palindrome Partitioning II

--- leetcode/132_Palindrome_Partitioning_II.go
+++ leetcode/132_Palindrome_Partitioning_II.go
@@ -15,9 +15,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(minCut("sdfkobj"))
+	fmt.Println(minCut("sdfoobj"))
 }
 func minCut(s string) int {
+	m := 0
+	palindromeMap := make(map[int]map[int]bool) // weather i to j is a palindrome
+	palindromeMapChild := make(map[int]bool)
+	minChar := make(map[int]int) // mini cut for 0 to i string
 
-	return 1
+	for i := 0; i < len(s); i++ {
+		m = i
+		for j := 0; j <= i; j++ {
+			if s[j:j+1] == s[i:i+1] {
+				_, v := palindromeMap[j+1][i-1]
+				palindromeMapChild[i] = false
+				if v == true || j+1 > i-1 {
+					palindromeMapChild[i] = true
+					palindromeMap[j] = palindromeMapChild
+					if j == 0 {
+						m = 0
+					} else {
+						if m > (minChar[j-1] + 1) {
+							m = minChar[j-1] + 1
+						}
+					}
+				}
+			}
+		}
+		minChar[i] = m
+	}
+	return minChar[len(s)-1]
 }
