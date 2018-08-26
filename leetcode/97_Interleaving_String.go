be85c1ac558eb6ee7feafb4bed26d8bd8d58edcc
frankerzeng <frankerzeng@163.com>
2018-08-26 19:56:12 +0800

    667. Beautiful Arrangement II

--- leetcode/97_Interleaving_String.go
+++ leetcode/97_Interleaving_String.go
@@ -16,45 +16,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(isInterleave("aabcc",
-		"dbbc a",
-		"aadbbcbca c"))
+	//fmt.Println(isInterleave("aabcc", "dbbca", "aadbbcbcac"))
+	fmt.Println(isInterleave("aabcc", "dbbca", "aadbbbaccc"))
 
 }
 func isInterleave(s1 string, s2 string, s3 string) bool {
-	s3Index := 0 // the comparing character index of s3
-	s2Index := 0 // the comparing character index of s2
-	s1Index := 0 // the comparing character index of s1
-	s3Len := len(s3)
-	s2Len := len(s2)
-	s1Len := len(s1)
-	for true {
-		if s3Index == s3Len-1 {
+	//s3Index  the comparing character index of s3
+	//s2Index  the comparing character index of s2
+	//s1Index  the comparing character index of s1
+	if len(s1)+len(s1) != len(s3) {
+		return false
+	}
+	return dpFunc(s1, s2, s3, 0, 0, 0)
+}
+func dpFunc(s1 string, s2 string, s3 string, s1Index int, s2Index int, s3Index int) bool {
+	if s3Index == len(s3) {
+		return true
+	}
+	if s1Index < len(s1) {
+		if s1[s1Index:s1Index+1] == s3[s3Index:s3Index+1] && dpFunc(s1, s2, s3, s1Index+1, s2Index, s3Index+1) {
 			return true
 		}
-		fmt.Println(s1Index, s2Index, s3Index)
-
-		if s1Index < s1Len && s3[s3Index:s3Index+1] == s1[s1Index:s1Index+1] {
-			s3Index++
-			s1Index++
-			continue
-		}
-		if s2Index < s2Len && s3[s3Index:s3Index+1] == s2[s2Index:s2Index+1] {
-			s3Index++
-			s2Index++
-			continue
-		}
-
-		if s3Index > 1 && s1Index > 1 {
-			if s3[s3Index-1:s3Index] == s2[s1Index-1:s1Index] && s3[s3Index-1:s3Index] == s2[s2Index-1:s2Index] {
-				s2Index++
-				s1Index--
-			} else {
-
-			}
-		}
-		fmt.Println("---------")
-
 	}
-	return true
+	if s2Index < len(s2) {
+		if s2[s2Index:s2Index+1] == s3[s3Index:s3Index+1] && dpFunc(s1, s2, s3, s1Index, s2Index+1, s3Index+1) {
+			return true
+		}
+	}
+	return false
 }
