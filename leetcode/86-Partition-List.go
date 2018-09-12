eca6ab551e923fec55d37642e745fe4f3d8627b0
frankerzeng <frankerzeng@163.com>
2018-09-12 18:43:17 +0800

    86. Partition List

--- leetcode/86-Partition-List.go
+++ leetcode/86-Partition-List.go
@@ -18,7 +18,13 @@ type ListNode = Common.ListNode
 func main() {
 	// 测试数据
 	var node1 *ListNode = new(ListNode)
-	node1 = nil
+	var node2 *ListNode = new(ListNode)
+	var node3 *ListNode = new(ListNode)
+	node1.Val = 1
+	node2.Val = 5
+	node3.Val = 3
+	node1.Next = node2
+	node2.Next = node3
 
 	fmt.Println(partition(node1, 2))
 }
@@ -31,7 +37,53 @@ func main() {
  * }
  */
 func partition(head *ListNode, x int) *ListNode {
+	var arrLess []int
+	var arrGreat []int
 
-	fmt.Println(12)
-	return head
+	if head == nil {
+		return head
+	}
+	var currentNods []*ListNode
+	currentNods = append(currentNods, head)
+	tmpNode := new(ListNode)
+	tmpNode = head
+
+	for true {
+		if tmpNode.Val < x {
+			arrLess = append(arrLess, tmpNode.Val)
+		} else {
+			arrGreat = append(arrGreat, tmpNode.Val)
+		}
+		if tmpNode.Next == nil {
+			break
+		}
+		tmpNode = tmpNode.Next
+	}
+	returnNode := new(ListNode)
+	returnNode = nil
+	for i := len(arrGreat) - 1; i >= 0; i-- {
+		tmpNode = new(ListNode)
+		tmpNode.Val = arrGreat[i]
+		if returnNode == nil {
+			tmpNode.Next = nil
+			returnNode = tmpNode
+		} else {
+			tmpNode.Next = returnNode
+			returnNode = tmpNode
+		}
+	}
+
+	for i := len(arrLess) - 1; i >= 0; i-- {
+		tmpNode = new(ListNode)
+		tmpNode.Val = arrLess[i]
+		if returnNode == nil {
+			tmpNode.Next = nil
+			returnNode = tmpNode
+		} else {
+			tmpNode.Next = returnNode
+			returnNode = tmpNode
+		}
+	}
+
+	return returnNode
 }
