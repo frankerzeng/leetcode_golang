8e2f081b90da932a4b7bb580071382681fe6ffe8
frankerzeng <frankerzeng@163.com>
2018-11-20 11:00:39 +0800

    2. Add Two Numbers list node

--- leetcode/2-Add-Two-Numbers.go
+++ leetcode/2-Add-Two-Numbers.go
@@ -16,17 +16,19 @@ package main
 import (
 	"../Common"
 	"fmt"
+	"strconv"
 )
 
 type ListNode = Common.ListNode
 
 func main() {
 	l1 := &ListNode{
-		Val: 2,
+		Val: 5,
 		Next: &ListNode{
-			Val: 4,
+			Val: 5,
 			Next: &ListNode{
-				Val: 3,
+				Val:  5,
+				Next: nil,
 			},
 		},
 	}
@@ -35,7 +37,8 @@ func main() {
 		Next: &ListNode{
 			Val: 6,
 			Next: &ListNode{
-				Val: 4,
+				Val:  4,
+				Next: nil,
 			},
 		},
 	}
@@ -50,6 +53,72 @@ func main() {
  *     Next *ListNode
  * }
  */
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+// Explanation: 342 + 465 = 807.
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	tmp2 := ""
+	flag := false // 是否进位
+	for true {
+		if l1 == nil && l2 == nil {
+			break
+		}
+		i1 := 0
+		i2 := 0
+		if l1 != nil {
+			i1 = l1.Val
+		}
+		if l2 != nil {
+			i2 = l2.Val
+		}
+		i12 := i1 + i2
+		if flag {
+			i12++
+		}
+		if i12 > 9 {
+			flag = true
+		} else {
+			flag = false
+		}
 
+		if l1 != nil {
+			l1 = l1.Next
+		} else {
+			l1 = nil
+		}
+		if l2 != nil {
+			l2 = l2.Next
+		} else {
+			l2 = nil
+		}
+		tmp2 = strconv.Itoa(i12%10) + tmp2
+	}
+	if flag {
+		tmp2 = "1" + tmp2
+	}
+
+	rst := &ListNode{}
+	for i := 0; i < len(tmp2); i++ {
+		strTmp, _ := strconv.Atoi(tmp2[i : i+1])
+		if i == 0 {
+			rst = &ListNode{
+				Val: strTmp,
+			}
+		} else {
+			rst = &ListNode{
+				Val:  strTmp,
+				Next: rst,
+			}
+		}
+	}
+
+	tmp1 := rst
+	for true {
+		if tmp1.Next != nil {
+			tmp1 = tmp1.Next
+		} else {
+			break
+		}
+	}
+	return rst
 }
