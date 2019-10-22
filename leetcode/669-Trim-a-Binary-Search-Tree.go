8d2312ac702d11ec0423bbbf6e28272f438a9c07
frankerzeng <frankerzeng@163.com>
2019-10-22 12:22:42 +0800

    669-Trim-a-Binary-Search-Tree.go

---
+++ leetcode/669-Trim-a-Binary-Search-Tree.go
@@ -0,0 +1,82 @@
+/**
+669. Trim a Binary Search Tree
+Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
+
+Example 1:
+Input:
+    1
+   / \
+  0   2
+
+  L = 1
+  R = 2
+
+Output:
+    1
+      \
+       2
+Example 2:
+Input:
+    3
+   / \
+  0   4
+   \
+    2
+   /
+  1
+
+  L = 1
+  R = 3
+
+Output:
+      3
+     /
+   2
+  /
+ 1
+*/
+package main
+
+import "fmt"
+
+func main() {
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   0,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+	}
+	rst := trimBST(root, 1, 2)
+	fmt.Println(rst)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func trimBST(root *TreeNode, L int, R int) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	if root.Val < L {
+		return trimBST(root.Right, L, R)
+	}
+	if root.Val > R {
+		return trimBST(root.Left, L, R)
+	}
+
+	root.Left = trimBST(root.Left, L, R)
+	root.Right = trimBST(root.Right, L, R)
+
+	return root
+}
